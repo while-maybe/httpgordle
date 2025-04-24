4d0553d659383c71e03c9655200ac34208a67d50
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-24 17:44:11 +0100

    test: add uTest for GetStatus handler

---
+++ internal/handlers/getstatus/handler_test.go
@@ -0,0 +1,27 @@
+package getstatus
+
+import (
+	"httpgordle/internal/api"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandle(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "/games/", nil)
+	require.NoError(t, err)
+
+	// add a path parameter
+	req.SetPathValue(api.GameID, "123456")
+
+	recorder := httptest.NewRecorder()
+
+	Handle(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+	assert.JSONEq(t, `{"id":"123456","attempts_left":0,"guesses":null,"word_length":0,"status":""}`, recorder.Body.String())
+}
