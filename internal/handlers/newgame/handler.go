dd1b3b4e2e02dfa149445bd0bd5ad74161433383
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-26 03:49:15 +0100

    refactor: separate responsabilities in new game creation Handler

--- internal/handlers/newgame/handler.go
+++ internal/handlers/newgame/handler.go
@@ -3,17 +3,34 @@ package newgame
 import (
 	"encoding/json"
 	"httpgordle/internal/api"
+	"httpgordle/internal/session"
 	"log"
 	"net/http"
 )
 
 func Handle(w http.ResponseWriter, req *http.Request) {
+	game, err := CreateGame()
+	if err != nil {
+		log.Printf("unable to create a new game: %s", err)
+		http.Error(w, "failed to create a new game", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	apiGame := api.GameResponse{}
-	err := json.NewEncoder(w).Encode(apiGame)
+	apiGame := response(game)
+
+	err = json.NewEncoder(w).Encode(apiGame)
 	if err != nil {
 		log.Printf("failed to write response: %s", err)
 	}
+}
+
+func CreateGame() (session.Game, error) {
+	return session.Game{}, nil
+}
+
+func response(game session.Game) api.GameResponse {
+	return api.GameResponse{}
 }
