f7619c9ce502a0639f724150842cb06e3b0175c8
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-26 17:28:41 +0100

    feat: add converter function from session.Game to GameResponse

---
+++ internal/api/convert.go
@@ -0,0 +1,25 @@
+package api
+
+import "httpgordle/internal/session"
+
+// ToGameResponse converts a session.Game into a GameResponse.
+func ToGameResponse(g session.Game) GameResponse {
+	apiGame := GameResponse{
+		ID:           string(g.ID),
+		AttemptsLeft: g.AttemptsLeft,
+		Guesses:      make([]Guess, len(g.Guesses)),
+		Status:       string(g.Status),
+		// TODO word length
+	}
+
+	for i := range g.Guesses {
+		apiGame.Guesses[i].Word = g.Guesses[i].Word
+		apiGame.Guesses[i].Feedback = g.Guesses[i].Feedback
+	}
+
+	if g.AttemptsLeft == 0 {
+		apiGame.Solution = "" // TODO solution
+	}
+
+	return apiGame
+}
