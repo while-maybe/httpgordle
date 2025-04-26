2ae92700ba8b4550e6995f12f5ce60c2b019975c
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-26 01:08:36 +0100

    feat: add session management package and data types

---
+++ internal/session/game.go
@@ -0,0 +1,32 @@
+package session
+
+import "errors"
+
+// Game contains the information about a game.
+type Game struct {
+	ID           GameID
+	AttemptsLeft byte
+	Guesses      []Guess
+	Status       Status
+}
+
+// GameID represents the ID of a game
+type GameID string
+
+// Status is the current status of the game and tells what operations can be made on it
+type Status string
+
+const (
+	StatusPlaying = "Playing"
+	StatusWon     = "Won"
+	StatusLost    = "Lost"
+)
+
+// A Guess is a (user-submitted) word and (Gordle generated) feedback pair.
+type Guess struct {
+	Word     string
+	Feedback string
+}
+
+// ErrGameOver is returned whan a play is made but the game is already over.
+var ErrGameOver = errors.New("game over")
