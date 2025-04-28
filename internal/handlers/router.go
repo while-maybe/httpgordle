e657d26ad9958bae9e157a200ed80532216e1191
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-28 04:15:32 +0100

    feat: implement db dependency injection

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -5,17 +5,20 @@ import (
 	"httpgordle/internal/handlers/getstatus"
 	"httpgordle/internal/handlers/guess"
 	"httpgordle/internal/handlers/newgame"
+	"httpgordle/internal/repository"
 	"net/http"
 )
 
 // NewRouter returns a new router that listens for requests to the following endpoints:
 // - Create a new game
 // - Get the status of a game
+// - Make a guess in the game
 
-func NewRouter() *http.ServeMux {
+func NewRouter(db *repository.GameRepository) *http.ServeMux {
 	r := http.NewServeMux()
-	r.HandleFunc(http.MethodPost+" "+api.NewGameRoute, newgame.Handle)
-	r.HandleFunc(http.MethodGet+" "+api.GetStatusRoute, getstatus.Handle)
-	r.HandleFunc(http.MethodPut+" "+api.GuessRoute, guess.Handle)
+
+	r.HandleFunc(http.MethodPost+" "+api.NewGameRoute, newgame.Handler(db))
+	r.HandleFunc(http.MethodGet+" "+api.GetStatusRoute, getstatus.Handler(db))
+	r.HandleFunc(http.MethodPut+" "+api.GuessRoute, guess.Handler(db))
 	return r
 }
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"httpgordle/internal/handlers"
+	"httpgordle/internal/repository"
 	"net/http"
 )
 
 func main() {
-	err := http.ListenAndServe(":8123", handlers.NewRouter())
+	db := repository.New()
+
+	err := http.ListenAndServe(":8123", handlers.NewRouter(db))
 	if err != nil {
 		panic(err)
 	}
