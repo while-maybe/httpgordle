db1956c94f767b528b0cd32bcdb08f50f10b4960
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-01 02:02:46 +0100

    feat: read from file once, read from buffer otherwise

--- internal/gordle/corpus.go
+++ internal/gordle/corpus.go
@@ -3,6 +3,7 @@ package gordle
 import (
 	_ "embed"
 	"fmt"
+	"log"
 	"math/rand/v2"
 	"os"
 	"strings"
@@ -13,15 +14,22 @@ const (
 	ErrInaccessibleCorpus = corpusError("corpus can't be opened")
 )
 
+var words []string
+
 // ReadCorpus reads the file located at the given path and returns a list of words.
 func ReadCorpus(path string) ([]string, error) {
+	if words != nil {
+		return words, nil
+	}
+
+	log.Printf("Opening %s", path)
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("unable to open %q for reading (%s): %w", path, err, ErrInaccessibleCorpus)
 	}
 
 	// we expect the corpus to be a line or space-separated list of words
-	words := strings.Fields(string(data))
+	words = strings.Fields(string(data))
 
 	if len(words) == 0 {
 		return nil, ErrEmptyCorpus
