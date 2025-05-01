fe7fdb0ba00f5194fc946c6b310e083d40d8e359
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-01 19:06:45 +0100

    refactor: update ToGameResponse to correctly set WordLength and Solution

--- internal/api/convert.go
+++ internal/api/convert.go
@@ -4,13 +4,14 @@ import "httpgordle/internal/session"
 
 // ToGameResponse converts a session.Game into a GameResponse.
 func ToGameResponse(g session.Game) GameResponse {
+	solution := g.Gordle.ShowAnswer()
+
 	apiGame := GameResponse{
 		ID:           string(g.ID),
 		AttemptsLeft: g.AttemptsLeft,
 		Guesses:      make([]Guess, len(g.Guesses)),
 		Status:       string(g.Status),
-		WordLength:   g.AttemptsLeft - 1,
-		// TODO word length
+		WordLength:   byte(len(solution)),
 	}
 
 	for i := range g.Guesses {
@@ -19,7 +20,7 @@ func ToGameResponse(g session.Game) GameResponse {
 	}
 
 	if g.AttemptsLeft == 0 {
-		apiGame.Solution = "" // TODO solution
+		apiGame.Solution = solution
 	}
 
 	return apiGame
