7f934b9e036f50223a0f733d23e45a3542578116
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-01 04:02:39 +0100

    wip: add uTests for newgame endpoint

--- internal/handlers/newgame/handler_test.go
+++ internal/handlers/newgame/handler_test.go
@@ -1,34 +1,81 @@
 package newgame
 
 import (
+	"httpgordle/internal/api"
 	"httpgordle/internal/session"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-type gameAdderStub struct {
+type gameCreatorStub struct {
 	err error
 }
 
-func (g gameAdderStub) Add(_ session.Game) error {
+func (g gameCreatorStub) Add(_ session.Game) error {
 	return g.err
 }
 
-func TestHandle(t *testing.T) {
-	handleFunc := Handler(gameAdderStub{})
+func TestHandler(t *testing.T) {
+	idFinderRegExp := regexp.MustCompile(`.+"id":"([0-9A-Z]+)".+`)
 
-	req, err := http.NewRequest(http.MethodPost, "/games", nil)
+	tt := map[string]struct {
+		wantStatusCode int
+		wantBody       string
+		creator        gameAdder
+	}{
+		"nominal": {
+			wantStatusCode: http.StatusCreated,
+			wantBody:       `{"id":"123456","attempts_left":5,"guesses":[],"word_length":4,"status":"Playing"}`,
+			creator:        gameCreatorStub{err: nil},
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := Handler(tc.creator)
+
+			req, err := http.NewRequest(http.MethodPost, api.NewGameRoute, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			recorder := httptest.NewRecorder()
+			f.ServeHTTP(recorder, req)
+
+			assert.Equal(t, tc.wantStatusCode, recorder.Code)
+
+			if tc.wantBody == "" {
+				return
+			}
+
+			body := recorder.Body.String()
+			id := idFinderRegExp.FindStringSubmatch(body)
+
+			if len(id) != 2 {
+				t.Fatal("cannot find id in json output")
+			}
+			body = strings.Replace(body, id[1], "123456", 1)
+
+			assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+			assert.JSONEq(t, tc.wantBody, body)
+		})
+	}
+
+}
+
+func Test_createGame(t *testing.T) {
+	corpusPath := "testdata/corpus.txt"
+
+	g, err := createGame(gameCreatorStub{nil}, corpusPath)
 	require.NoError(t, err)
 
-	recorder := httptest.NewRecorder()
-
-	handleFunc(recorder, req)
-
-	assert.Equal(t, http.StatusCreated, recorder.Code)
-	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
-	assert.JSONEq(t, `{"id":"","attempts_left":0,"guesses":[],"word_length":0,"status":""}`, recorder.Body.String())
+	assert.Regexp(t, "[A-Z0-9]+", g.ID)
+	assert.Equal(t, byte(5), g.AttemptsLeft)
+	assert.Equal(t, 0, len(g.Guesses))
 }
